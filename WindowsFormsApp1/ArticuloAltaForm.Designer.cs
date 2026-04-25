c2c565638aca07d042ab9b5559f04e9f4162ad22
alanC <alancaero57@gmail.com>
2026-04-25 17:09:50 -0300

    ArticuloAltaForm - Modelado

--- WindowsFormsApp1/ArticuloAltaForm.Designer.cs
+++ WindowsFormsApp1/ArticuloAltaForm.Designer.cs
@@ -28,20 +28,256 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpBoxAltaArticulo = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.cboMarca = new System.Windows.Forms.ComboBox();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.pctImagen = new System.Windows.Forms.PictureBox();
+            this.lblImagen = new System.Windows.Forms.Label();
+            this.pnlInferior = new System.Windows.Forms.Panel();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.grpBoxAltaArticulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).BeginInit();
+            this.pnlInferior.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // grpBoxAltaArticulo
+            // 
+            this.grpBoxAltaArticulo.Controls.Add(this.cboCategoria);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblImagen);
+            this.grpBoxAltaArticulo.Controls.Add(this.cboMarca);
+            this.grpBoxAltaArticulo.Controls.Add(this.textBox2);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblDescripcion);
+            this.grpBoxAltaArticulo.Controls.Add(this.txtPrecio);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblPrecio);
+            this.grpBoxAltaArticulo.Controls.Add(this.txtCodigo);
+            this.grpBoxAltaArticulo.Controls.Add(this.txtNombre);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblCategoria);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblMarca);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblNombre);
+            this.grpBoxAltaArticulo.Controls.Add(this.lblCodigo);
+            this.grpBoxAltaArticulo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpBoxAltaArticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpBoxAltaArticulo.Location = new System.Drawing.Point(0, 0);
+            this.grpBoxAltaArticulo.Name = "grpBoxAltaArticulo";
+            this.grpBoxAltaArticulo.Size = new System.Drawing.Size(951, 518);
+            this.grpBoxAltaArticulo.TabIndex = 2;
+            this.grpBoxAltaArticulo.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(123, 239);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(229, 22);
+            this.textBox2.TabIndex = 14;
+            this.textBox2.TabStop = false;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(15, 239);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(79, 16);
+            this.lblDescripcion.TabIndex = 13;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(123, 193);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.ReadOnly = true;
+            this.txtPrecio.Size = new System.Drawing.Size(229, 22);
+            this.txtPrecio.TabIndex = 12;
+            this.txtPrecio.TabStop = false;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(15, 193);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(49, 16);
+            this.lblPrecio.TabIndex = 11;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(123, 37);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.ReadOnly = true;
+            this.txtCodigo.Size = new System.Drawing.Size(229, 22);
+            this.txtCodigo.TabIndex = 8;
+            this.txtCodigo.TabStop = false;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(123, 74);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.ReadOnly = true;
+            this.txtNombre.Size = new System.Drawing.Size(229, 22);
+            this.txtNombre.TabIndex = 5;
+            this.txtNombre.TabStop = false;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(15, 153);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(69, 16);
+            this.lblCategoria.TabIndex = 3;
+            this.lblCategoria.Text = "Categoría:";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMarca.Location = new System.Drawing.Point(15, 116);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(48, 16);
+            this.lblMarca.TabIndex = 2;
+            this.lblMarca.Text = "Marca:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(15, 74);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.Location = new System.Drawing.Point(15, 37);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(54, 16);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // cboMarca
+            // 
+            this.cboMarca.FormattingEnabled = true;
+            this.cboMarca.Location = new System.Drawing.Point(123, 116);
+            this.cboMarca.Name = "cboMarca";
+            this.cboMarca.Size = new System.Drawing.Size(229, 24);
+            this.cboMarca.TabIndex = 15;
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Location = new System.Drawing.Point(123, 153);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(229, 24);
+            this.cboCategoria.TabIndex = 16;
+            // 
+            // pctImagen
+            // 
+            this.pctImagen.Location = new System.Drawing.Point(460, 56);
+            this.pctImagen.Name = "pctImagen";
+            this.pctImagen.Size = new System.Drawing.Size(349, 356);
+            this.pctImagen.TabIndex = 3;
+            this.pctImagen.TabStop = false;
+            // 
+            // lblImagen
+            // 
+            this.lblImagen.AutoSize = true;
+            this.lblImagen.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImagen.Location = new System.Drawing.Point(605, 18);
+            this.lblImagen.Name = "lblImagen";
+            this.lblImagen.Size = new System.Drawing.Size(55, 16);
+            this.lblImagen.TabIndex = 14;
+            this.lblImagen.Text = "Imagen:";
+            // 
+            // pnlInferior
+            // 
+            this.pnlInferior.Controls.Add(this.btnGuardar);
+            this.pnlInferior.Controls.Add(this.btnCancelar);
+            this.pnlInferior.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlInferior.Location = new System.Drawing.Point(0, 472);
+            this.pnlInferior.Name = "pnlInferior";
+            this.pnlInferior.Size = new System.Drawing.Size(951, 46);
+            this.pnlInferior.TabIndex = 15;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGuardar.AutoSize = true;
+            this.btnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.Location = new System.Drawing.Point(770, 11);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(88, 26);
+            this.btnGuardar.TabIndex = 1;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancelar.AutoSize = true;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(860, 11);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(79, 26);
+            this.btnCancelar.TabIndex = 0;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
             // 
             // ArticuloAltaForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(951, 518);
+            this.Controls.Add(this.pnlInferior);
+            this.Controls.Add(this.pctImagen);
+            this.Controls.Add(this.grpBoxAltaArticulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ArticuloAltaForm";
             this.Text = "ArticuloAltaForm";
+            this.grpBoxAltaArticulo.ResumeLayout(false);
+            this.grpBoxAltaArticulo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctImagen)).EndInit();
+            this.pnlInferior.ResumeLayout(false);
+            this.pnlInferior.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox grpBoxAltaArticulo;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.ComboBox cboMarca;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.PictureBox pctImagen;
+        private System.Windows.Forms.Label lblImagen;
+        private System.Windows.Forms.Panel pnlInferior;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnCancelar;
     }
 }
