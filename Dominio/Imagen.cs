92694f882f405089e02e8b0c39da97c6ec227a40
alanC <alancaero57@gmail.com>
2026-04-24 22:39:34 -0300

    Add - Clases base

---
+++ Dominio/Imagen.cs
@@ -0,0 +1,9 @@
+﻿namespace Dominio
+{
+    public class Imagen
+    {        
+        public int Id { get; set; }
+        public Articulo Articulo { get; set; }
+        public string ImagenUrl { get; set; }        
+    }
+}
